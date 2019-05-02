4ea75fa1bce61633d8bb29809ed78bd42025bb14
Syann Hollins <02syann.hollins@gulllakecs.org>
2019-05-02 12:57:30 -0400

    Setting cursor

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -14,7 +14,14 @@ long duration;
 int distanceCm, distanceInch;
 
 void setup() {
-  // put your setup code here, to run once:
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2); // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0
+  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 1);
+  // (connecting LCD to the sensor):
+  pinMode(trigPin, OUTPUT);
+  
 
 }
 
