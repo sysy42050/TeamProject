51614bc4c9dc9ff97a287f986984c535be27ee54
02kristianshyiak <48028427+02kristianshyiak@users.noreply.github.com>
2019-05-20 12:56:50 -0400

    Update LCD.ino

--- Kristian-Syann_Distance_Sensor/LCD/LCD.ino
+++ Kristian-Syann_Distance_Sensor/LCD/LCD.ino
@@ -26,6 +26,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+ 
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+distanceInch = duration*0.0133/2;
+ 
+lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
+lcd.print("Distance: "); // Prints string "Distance" on the LCD
+lcd.print(distanceCm); // Prints the distance value from the sensor
+lcd.print("  cm");
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Distance: ");
+lcd.print(distanceInch);
+lcd.print("inch");
+delay(10);
 
 }
